7871875461982f6dd675054720914be7cd9e7ec7
haha27lim <eileen2708@hotmail.com>
2022-12-01 23:23:43 +0800

    initial commit

---
+++ src/myapp/MyFirstApp.java
@@ -0,0 +1,12 @@
+package myapp;
+
+//class definition
+public class MyFirstApp {
+
+    //Entry point
+    public static void main(String[] args) {
+        System.out.println("hello, World");
+        System.out.println("floorbar");
+    }
+    
+}
